32f7c3b61ab6af47a90562b27509932126246959
Andrea <drealvarez97@gmail.com>
2020-10-03 19:11:41 +0200

    Reformat code #17

--- src/test/java/code/FractionTest.java
+++ src/test/java/code/FractionTest.java
@@ -13,108 +13,108 @@ class FractionTest {
     private Fraction fraction4;
 
     @BeforeEach
-    void before(){
-        fraction = new Fraction(2,5);
-        fraction2 = new Fraction(1,3);
-        fraction3 = new Fraction(1,3);
-        fraction4 = new Fraction(4,3);
+    void before() {
+        fraction = new Fraction(2, 5);
+        fraction2 = new Fraction(1, 3);
+        fraction3 = new Fraction(1, 3);
+        fraction4 = new Fraction(4, 3);
     }
 
     @Test
-    void testFraction(){
-        assertEquals(new Fraction(1,1),new Fraction());
+    void testFraction() {
+        assertEquals(new Fraction(1, 1), new Fraction());
     }
 
     @Test
-    void testGetNumerator(){
-        assertEquals(2,fraction.getNumerator());
-        assertEquals(1,fraction2.getNumerator());
+    void testGetNumerator() {
+        assertEquals(2, fraction.getNumerator());
+        assertEquals(1, fraction2.getNumerator());
     }
 
     @Test
-    void testSetNumerator(){
+    void testSetNumerator() {
         fraction.setNumerator(3);
-        assertEquals(3,fraction.getNumerator());
+        assertEquals(3, fraction.getNumerator());
     }
 
     @Test
-    void testGetDenominator(){
-        assertEquals(5,fraction.getDenominator());
-        assertEquals(3,fraction2.getDenominator());
+    void testGetDenominator() {
+        assertEquals(5, fraction.getDenominator());
+        assertEquals(3, fraction2.getDenominator());
     }
 
     @Test
-    void testSetDenominator(){
+    void testSetDenominator() {
         fraction.setDenominator(10);
-        assertEquals(10,fraction.getDenominator());
+        assertEquals(10, fraction.getDenominator());
     }
 
     @Test
-    void testDecimal(){
-        assertEquals(0.4,fraction.decimal());
+    void testDecimal() {
+        assertEquals(0.4, fraction.decimal());
     }
 
     @Test
-    void testIsProper(){
+    void testIsProper() {
         assertTrue(fraction.isProper());
         assertFalse(fraction4.isProper());
     }
 
     @Test
-    void testIsImproper(){
+    void testIsImproper() {
         assertFalse(fraction.isImproper());
         assertTrue(fraction4.isImproper());
     }
 
     @Test
-    void testIsEquivalent(){
+    void testIsEquivalent() {
         assertFalse(fraction.isEquivalent(fraction2));
         assertTrue(fraction2.isEquivalent(fraction3));
     }
 
     @Test
-    void testAdd(){
-        assertEquals(new Fraction(11,15),fraction.add(fraction2));
-        assertEquals(new Fraction(6,9),fraction2.add(fraction2));
+    void testAdd() {
+        assertEquals(new Fraction(11, 15), fraction.add(fraction2));
+        assertEquals(new Fraction(6, 9), fraction2.add(fraction2));
     }
 
     @Test
-    void testMultiply(){
-        assertEquals(new Fraction(2,15),fraction.multiply(fraction2));
+    void testMultiply() {
+        assertEquals(new Fraction(2, 15), fraction.multiply(fraction2));
     }
 
     @Test
-    void testDivide(){
-        assertEquals(new Fraction(6,5),fraction.divide(fraction2));
-        assertEquals(new Fraction(5,6),fraction2.divide(fraction));
+    void testDivide() {
+        assertEquals(new Fraction(6, 5), fraction.divide(fraction2));
+        assertEquals(new Fraction(5, 6), fraction2.divide(fraction));
     }
 
     @Test
-    void testIsHigher(){
-        assertEquals(fraction4,fraction.isHigher(fraction4));
-        assertEquals(fraction4,fraction4.isHigher(fraction2));
+    void testIsHigher() {
+        assertEquals(fraction4, fraction.isHigher(fraction4));
+        assertEquals(fraction4, fraction4.isHigher(fraction2));
     }
 
     @Test
-    void testEquals(){
+    void testEquals() {
         assertEquals(fraction2, fraction3);
         assertNotEquals(fraction2, fraction);
-        assertEquals(false,fraction.equals(null));
+        assertEquals(false, fraction.equals(null));
     }
 
     @Test
-    void testHashcode(){
-        assertEquals(fraction2.hashCode(),fraction3.hashCode());
-        assertNotEquals(fraction2.hashCode(),fraction.hashCode());
+    void testHashcode() {
+        assertEquals(fraction2.hashCode(), fraction3.hashCode());
+        assertNotEquals(fraction2.hashCode(), fraction.hashCode());
     }
 
     @Test
-    void testToString(){
-        String temp= "Fraction{" +
+    void testToString() {
+        String temp = "Fraction{" +
                 "numerator=" + fraction.getNumerator() +
                 ", denominator=" + fraction.getDenominator() +
                 '}';
-        assertEquals(temp,fraction.toString());
+        assertEquals(temp, fraction.toString());
     }
 
 }
